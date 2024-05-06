461b0da56582406c6a1fcfafa04dcd213da0feff
Lucas Mol <lucasmolpro@outlook.com>
2024-05-05 22:55:44 -0300

    fix: :white_check_mark: fix intentional error to fail CI workflow
    fix function call on test file added to intentionally throw a error on latest commit

--- math_test.go
+++ math_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestSoma(t *testing.T) {
-	total := Sum(15, 15)
+	total := sum(15, 15)
 
 	if total != 30 {
 		t.Errorf("The sum result is incorrect: Result %d, Must be %d", total, 30)
